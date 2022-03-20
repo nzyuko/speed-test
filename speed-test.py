667d3fc292fa0692d60d46e2c69414059137f10b
nzyuko <johnnzyuko514@gmail.com>
2022-03-20 08:35:13 +0300

    imported colors class from the colors.py file

--- speed-test.py
+++ speed-test.py
@@ -1,16 +1,6 @@
+#!/usr/bin/python
 import speedtest
-
-
-class Colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-
+from colors import Colors
 
 test = speedtest.Speedtest()
 
@@ -26,6 +16,6 @@ print(Colors.OKBLUE + "Performing Upload Test ...")
 upload_result = test.upload()
 ping_result = test.results.ping
 
-print(Colors.OKGREEN + f"DOWNLOAD SPEED : {download_result/ 1024 /1024:.2f} Mbits/Second")
-print(Colors.OKGREEN + f"UPLOAD SPEED : {upload_result/ 1024 /1024:.2f} Mbits/Second")
+print(Colors.OKGREEN + f"DOWNLOAD SPEED : {download_result / 1024 / 1024:.2f} Mbits/Second")
+print(Colors.OKGREEN + f"UPLOAD SPEED : {upload_result / 1024 / 1024:.2f} Mbits/Second")
 print(Colors.OKGREEN + f"Ping : {ping_result:.2f} ms")
