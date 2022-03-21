f20a2f9afb6d2e9c7e324548337d83f71858e6c9
nzyuko <johnnzyuko514@gmail.com>
2022-03-21 15:21:58 +0300

    added a try catch block to alert user when an internet connection is not found

--- speed-test.py
+++ speed-test.py
@@ -4,18 +4,21 @@ from colors import Colors
 
 test = speedtest.Speedtest()
 
-print(Colors.OKBLUE + "Loading Server list ....")
-test.get_servers()
-print(Colors.OKBLUE + "Choosing best Sever ....")
-best = test.get_best_server()
-print(Colors.OKGREEN + f"Found: {best['host']} located in {best['country']}")
+try:
+    print(Colors.OKBLUE + "Loading Server list ....")
+    test.get_servers()
+    print(Colors.OKBLUE + "Choosing best Sever ....")
+    best = test.get_best_server()
+    print(Colors.OKGREEN + f"Found: {best['host']} located in {best['country']}")
 
-print(Colors.OKBLUE + "Performing Download Test ...")
-download_result = test.download()
-print(Colors.OKBLUE + "Performing Upload Test ...")
-upload_result = test.upload()
-ping_result = test.results.ping
+    print(Colors.OKBLUE + "Performing Download Test ...")
+    download_result = test.download()
+    print(Colors.OKBLUE + "Performing Upload Test ...")
+    upload_result = test.upload()
+    ping_result = test.results.ping
 
-print(Colors.OKGREEN + f"DOWNLOAD SPEED : {download_result / 1024 / 1024:.2f} Mbits/Second")
-print(Colors.OKGREEN + f"UPLOAD SPEED : {upload_result / 1024 / 1024:.2f} Mbits/Second")
-print(Colors.OKGREEN + f"Ping : {ping_result:.2f} ms")
+    print(Colors.OKGREEN + f"DOWNLOAD SPEED : {download_result / 1024 / 1024:.2f} Mbits/Second")
+    print(Colors.OKGREEN + f"UPLOAD SPEED : {upload_result / 1024 / 1024:.2f} Mbits/Second")
+    print(Colors.OKGREEN + f"Ping : {ping_result:.2f} ms")
+except ConnectionError:
+    print(Colors.WARNING + "Unable to connect to speed test server check your and try again")
